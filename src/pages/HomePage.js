4baccd1a287540ea0b8eaa67664a1c83d285b073
AbdulTawabJuly <tawabmasood31july@gmail.com>
2023-09-22 11:40:09 +0500

    Login/Signup Complete

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,23 @@
-import Navbar from "../features/Navbar/Navbar"
-import { selectLoggedInUser } from "../features/auth/authSlice";
+import { PacmanLoader } from "react-spinners";
+import Navbar from "../features/Navbar/Navbar";
+import { selectLoggedInUser, selectStatus } from "../features/auth/authSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 function HomePage() {
   const user = useSelector(selectLoggedInUser);
-
+  const status = useSelector(selectStatus);
+  console.log({ user: user });
   return (
     <div className=" min-h-screen bg-Auth-0">
-    <Navbar></Navbar>
+      <Navbar></Navbar>
       <h1 className="text-center text-4xl text-black font-Logo">Home</h1>
       <br />
-      <p>{user.email}</p>
+      <div className="flex justify-center items-center">
+        {status === "loading" ? (
+          <PacmanLoader color="#435334"/>
+        ) : null}
+      </div>
+      {user.user.email && <div className=" text-4xl">{user.user.email}</div>}
     </div>
   );
 }
