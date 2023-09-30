3f03970fc4b82502756c2e45f29aee8170ce6cee
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-09-30 20:47:05 +0500

    Add redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import storage from 'redux-persist/lib/storage'
+import {persistReducer} from 'redux-persist'
+import {comnineReducer} from '@reduxjs/toolkit'
+
+const persistConfig = {
+  key:"root",
+  version:1,
+  storage
+};
+
+const reducer = combineReducers({
+  auth:authReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+  reducer: persistedReducer
 });
