f952ac64648b7d1d8feabdcf804837b6d8f90525
Suhaib Rashid <suhaibrashid2003@gmail.com>
2023-12-03 19:48:14 +0500

    image update

--- src/features/rooms/components/RoomCard.js
+++ src/features/rooms/components/RoomCard.js
@@ -44,7 +44,7 @@ function RoomCard({ RoomDetails }) {
   };
   const handleProfileClick=(e)=>{
     e.stopPropagation();
-    navigate("/profile/"+RoomDetails.createdBy._id);
+    navigate("/profile/"+RoomDetails?.createdBy?._id);
   }
 
   //const handlePayment = () => {};
@@ -85,7 +85,7 @@ function RoomCard({ RoomDetails }) {
             <img
               onClick={(e)=>handleProfileClick(e)}
               className="w-7 h-7 border border-black rounded-full object-contain bg-black"
-              src={RoomDetails.createdBy.image}
+              src={RoomDetails?.createdBy?.image}
               alt="img"
             ></img>
           )}
