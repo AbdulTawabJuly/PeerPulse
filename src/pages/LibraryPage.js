c89798532b74905a8660776f65d28dddbe4cdc8f
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-03-14 22:17:12 +0500

    [feat] Added Main Component in Library Page

--- src/pages/LibraryPage.js
+++ src/pages/LibraryPage.js
@@ -1,11 +1,12 @@
 import Navbar from "../features/Navbar/Navbar"
 import TrendingContributers from "../features/virtualLibrary/components/TrendingContributers"
+import Main from "../features/virtualLibrary/components/Main"
 
 function LibraryPage() {
   return (
     <div className="bg-Primary-0" style={{ minHeight: "100vh" }}>
       <Navbar />
-      <TrendingContributers />
+      <Main />
     </div>
   )
 }
