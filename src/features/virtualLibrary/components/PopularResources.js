a995a2f68011bb46d8e03832a160da1ac1a78627
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-04-11 17:07:37 +0500

    [feat] Added Popular Resources Component to Library Page

--- src/features/virtualLibrary/components/PopularResources.js
+++ src/features/virtualLibrary/components/PopularResources.js
@@ -11,6 +11,7 @@ const PopularResources = () => {
             </div>
           </div>
         </div>
+
       </div>
     )
   }
--- src/pages/LibraryPage.js
+++ src/pages/LibraryPage.js
@@ -5,7 +5,7 @@ import Main from "../features/virtualLibrary/components/Main"
 
 function LibraryPage() {
   return (
-    <div className="bg-Primary-0" style={{ minHeight: "100vh" }}>
+    <div className="bg-Primary-0" style={{ height: "100vh" }}>
       <Navbar />
       <div className="grid grid-cols-7 gap-10 mt-8">
         <div className="col-span-1">
@@ -14,8 +14,9 @@ function LibraryPage() {
         <div className="col-span-4">
           <Main />
         </div>
-        <div className="col-span-2 mx-auto">
+        <div className="col-span-2 mx-auto flex flex-col justify-between">
           <TrendingContributers />
+          <PopularResources />
         </div>
       </div>
       
