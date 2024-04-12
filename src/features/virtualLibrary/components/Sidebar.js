51bf40adf6983261dd34fb1eb16fd21b75c3e18c
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-04-12 22:45:27 +0500

    [feat] Initiallized Sidebar

---
+++ src/features/virtualLibrary/components/Sidebar.js
@@ -0,0 +1,10 @@
+const Sidebar = () => {
+    return (
+        <div style={{height:'100%'}} className='bg-Main-0 rounded-lg font-body w-42 p-4'>
+            <h1>Sidebar</h1>
+
+        </div>
+    )
+}
+
+export default Sidebar
--- src/pages/LibraryPage.js
+++ src/pages/LibraryPage.js
@@ -1,15 +1,16 @@
 import Navbar from "../features/Navbar/Navbar"
 import TrendingContributers from "../features/virtualLibrary/components/TrendingContributers"
 import PopularResources from "../features/virtualLibrary/components/PopularResources"
+import Sidebar from "../features/virtualLibrary/components/Sidebar"
 import Main from "../features/virtualLibrary/components/Main"
 
 function LibraryPage() {
   return (
-    <div className="bg-Primary-0" style={{ height: "100vh" }}>
+    <div className="bg-Primary-0 overflow-scroll" style={{ height: "100vh" }}>
       <Navbar />
-      <div className="grid grid-cols-7 gap-10 mt-8">
-        <div className="col-span-1">
-          <h1>Sidebar</h1>
+      <div className="grid grid-cols-7 mt-8">
+        <div className="col-span-1 mx-4">
+          <Sidebar />
         </div>
         <div className="col-span-4">
           <Main />
