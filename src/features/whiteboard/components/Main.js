e01766b1cbd6d3d61ec0d045f4f2353df1df5d03
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-04-15 23:22:21 +0500

    [feat] Added Main Component to Whiteboard Page

--- src/features/whiteboard/components/Main.js
+++ src/features/whiteboard/components/Main.js
@@ -1,6 +1,6 @@
 function Main() {
     return (
-        <div className="main">
+        <div className="bg-Main-0 mx-auto mt-10 rounded-lg" style={{height:'90%', width:'90%'}}>
             <h1>Whiteboard</h1>
         </div>
     );
