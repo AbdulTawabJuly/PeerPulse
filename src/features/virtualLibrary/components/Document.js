5d489a3b6e02503b891cf8e04cabe80c28b4a9bf
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-05-04 18:16:18 +0500

    Added Get Documents Function to Library Page

--- src/features/virtualLibrary/components/Document.js
+++ src/features/virtualLibrary/components/Document.js
@@ -1,4 +1,4 @@
-const Document = () => {
+const Document = ({document}) => {
     return (
         <div className="w-32 relative mx-auto sm:mx-0">
             <div className="w-32 h-48 overflow-hidden">
@@ -9,8 +9,8 @@ const Document = () => {
             <div>
                 <div className="">
                     <div className="max-w-full flex-col p-1">
-                        <h2 className="text-wrap overflow-x-hidden text-Secondary-0">Novel</h2>
-                        <h2 className="text-wrap overflow-x-hidden font-light">Ameer Hamza</h2>
+                        <h2 className="text-wrap overflow-x-hidden text-Secondary-0">{document.name}</h2>
+                        <h2 className="text-wrap overflow-x-hidden font-light">{document.createdBy}</h2>
                     </div>
                 </div>
             </div>
